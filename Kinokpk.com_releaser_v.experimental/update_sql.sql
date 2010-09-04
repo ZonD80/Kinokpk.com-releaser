85b7e30c0b3eb6b4e063d2a3f3255a36134e00e0
ZonD80 <zond80@gmail.com>
2010-09-05 00:08:39 +0400

    Minor updates

--- Kinokpk.com_releaser_v.experimental/update_sql.sql
+++ Kinokpk.com_releaser_v.experimental/update_sql.sql
@@ -12,3 +12,4 @@ DELETE FROM `cache_stats` WHERE `cache_stats`.`cache_name` = 'use_lang';
 DELETE FROM `cache_stats` WHERE `cache_stats`.`cache_name` =  'use_sessions';
 DELETE FROM `cache_stats` WHERE `cache_stats`.`cache_name` =  'use_wait';
 update orbital_blocks set bposition='t' where bposition='c';
+update news set comments=(SELECT COUNT(*) FROM comments WHERE type='news' AND toid=news.id) where id=id
