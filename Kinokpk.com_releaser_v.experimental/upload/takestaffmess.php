d3ab430ed08a81cdb85a96e32fa85f144a96cbc4
ZonD80 <zond80@gmail.com>
2011-06-11 22:27:54 +0400

    Mass PM sender fix

--- Kinokpk.com_releaser_v.experimental/upload/takestaffmess.php
+++ Kinokpk.com_releaser_v.experimental/upload/takestaffmess.php
@@ -29,7 +29,7 @@ $subject = htmlspecialchars((string)$_POST['subject']);
 if (!$subject)
 stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('enter_subject'));
 
-if (!is_array($_POST['clases']))
+if (!is_array($_POST['classes']))
 stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('select_classes'));
 
 /*$query = sql_query("SELECT id FROM users WHERE class IN (".implode(", ", array_map("sqlesc", $clases)).")");
@@ -40,7 +40,7 @@ sql_query("INSERT INTO messages (sender, receiver, added, msg, subject) VALUES (
 
 write_log($REL_LANG->say_by_key('mass_mailing')." $CURUSER[username]","tracker");
 
-sql_query("INSERT INTO messages (sender, receiver, added, msg, subject) SELECT $sender_id, id, ".time().", ".sqlesc($msg).", ".sqlesc($subject)." FROM users WHERE class IN (".implode(", ", array_map("intval", $_POST['clases'])).")") or sqlerr(__FILE__,__LINE__);
+sql_query("INSERT INTO messages (sender, receiver, added, msg, subject) SELECT $sender_id, id, ".time().", ".sqlesc($msg).", ".sqlesc($subject)." FROM users WHERE class IN (".implode(", ", array_map("intval", $_POST['classes'])).")") or sqlerr(__FILE__,__LINE__);
 $counter = mysql_affected_rows();
 
 safe_redirect($REL_SEO->make_link('staffmess'),1);
