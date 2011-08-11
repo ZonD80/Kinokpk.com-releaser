462cff48dfb7c3d9497276e4ce274a5a74de02fb
ZonD80 <zond80@gmail.com>
2011-08-11 15:13:37 +0400

    Class administration (viewer only, no editing yet), you must create privilege "access_to_classadmin" via privileges admin panel to use classadmin.php

--- Kinokpk.com_releaser_v.experimental/upload/classadmin.php
+++ Kinokpk.com_releaser_v.experimental/upload/classadmin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Privileges administration panel
+ * Classes administration panel
  * @license GNU GPLv3 http://opensource.org/licenses/gpl-3.0.html
  * @package Kinokpk.com releaser
  * @author ZonD80 <admin@kinokpk.com>
@@ -13,15 +13,28 @@ INIT();
 loggedinorreturn();
 
 
-get_privilege('access_to_privadmincp');
+get_privilege('access_to_classadmin');
 
 httpauth();
 
 $a = (string)$_GET['a'];
 
-$reserved_privileges = array('is_guest');
+function rolesdesc() {
+	global $REL_TPL;
+	return $REL_TPL->fetch('modules/classadmin/rolesdesc.tpl');
+}
 
-if ($_SERVER['REQUEST_METHOD']=='POST') {
+function make_role_checkbox($currole,$name) {
+	
+	$roles = array('sysop','uploader','staffbegin','vip','rating','reg','guest');
+	$currole = explode(',',$currole);
+	foreach ($roles as $r) {
+		$return.='<input class="role_'.$r.'" type="checkbox" name="'.$name.'"'.(in_array($r, $currole)?' checked="checked"':'').'> '.$r.'&nbsp;';
+	}
+	return $return;
+}
+
+/*if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$name = trim((string)($_POST['name']?$_POST['name']:$_GET['name']));
 	if ($a=='add') {
 		if (!$_POST['classes']) $REL_TPL->stderr($REL_LANG->_('Error'),$REL_LANG->_('Error. Please select at least one class'));
@@ -48,31 +61,31 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 		die($REL_LANG->_('Privilege updated'));
 		}
 	}
-}
+}*/
 
-$REL_TPL->stdhead($REL_LANG->_('Privileges control panel'));
+$REL_TPL->stdhead($REL_LANG->_('Classes control panel'));
 
 if ($a=='add'||$a=='edit') {
-		$name = trim((string)($_POST['name']?$_POST['name']:$_GET['name']));
+		$id = (int)($_POST['id']?$_POST['id']:$_GET['id']);
 		if ($a=='edit') {
-			$p = $REL_DB->query_row("SELECT * FROM privileges WHERE name=".sqlesc($name));
-			if (!$p) {
-				$REL_TPL->stdmsg($REL_LANG->_('Error'),$REL_LANG->_('No privilege named "%s"',htmlspecialchars($name)),'error');
+			$c = $REL_DB->query_row("SELECT * FROM classes WHERE id=$id");
+			if (!$c) {
+				$REL_TPL->stdmsg($REL_LANG->_('Error'),$REL_LANG->_('Invalid id'));
 				$REL_TPL->stdfoot();
 				die();
 			}
-			$REL_TPL->assign('p',$p);
+			$REL_TPL->assign('c',$c);
 		}
 		$REL_TPL->assign('a',$a);
 	$REL_TPL->output('edit');
 } 
 else {
 
-		$privs = $REL_DB->query_return("SELECT * FROM privileges ORDER BY id DESC");
-		foreach ($privs AS $priv) {
-			$p[$priv['id']] = array('name'=>$priv['name'],'classes'=>$priv['classes_allowed'],'descr'=>$priv['description']);
+		$classes = $REL_DB->query_return("SELECT * FROM classes ORDER BY prior DESC");
+		foreach ($classes AS $class) {
+			$c[$class['id']] = $class;
 		}
-	$REL_TPL->assign('p',$p);
+	$REL_TPL->assign('c',$c);
 	$REL_TPL->output();	
 }
 $REL_TPL->stdfoot();
---
+++ Kinokpk.com_releaser_v.experimental/upload/themes/releaser330/modules/classadmin/index.tpl
@@ -0,0 +1,29 @@
+{literal}
+<script type="text/javascript">
+
+
+$(document).ready(function(){
+	$(":checkbox").click(function(){
+		cl = $(this).attr('class');
+		//alert(cl);
+		$(this).attr('class','current');
+		$('.'+cl).each(function(){
+			//alert('test');
+			$(this).attr('checked', false);
+			});
+		$(this).attr('class',cl);
+	});
+});
+</script>
+{/literal}
+<form method="post" action="{$REL_SEO->make_link('classadmin','a','reorder')}">
+<table border="1" width="100%">
+<tr><td colspan="6">{rolesdesc()}</td></tr>
+<tr><td colspan="6"><h1>{$REL_LANG->_('Current classes')} | <a href="{$REL_SEO->make_link('classadmin','a','add')}">{$REL_LANG->_('Add')}</a></h1></td></tr>
+<tr><td class="colhead">ID</td><td class="colhead">{$REL_LANG->_('Name')}</td><td class="colhead">{$REL_LANG->_('Priority')}</td><td class="colhead">{$REL_LANG->_('Style')}</td><td class="colhead">{$REL_LANG->_('Roles')}</td><td class="colhead">{$REL_LANG->_('Actions')}</td></tr>
+{foreach item=cl from=$c}
+<tr id="tr-{$cl.id}"><td>{$cl.id}</td><td>{$REL_LANG->_($cl.name)}</td><td><input type="text" name="prior[{$cl.id}]" size="3" value="{$cl.prior}"/></td><td>{if $cl.style}{$cl.style|htmlentities}{else}{$REL_LANG->_('No')}{/if}</td><td>{make_role_checkbox($cl.remark,"role[{$cl.id}]")}</td><td><a href="{$REL_SEO->make_link('classadmin','a','edit','id',$cl.id)}">{$REL_LANG->_('Edit')}</a> / <a href="{$REL_SEO->make_link('classadmin','a','delete','id',$cl.id)}">{$REL_LANG->_('Delete')}</a></td></tr>
+{/foreach}
+<tr><td colspan="6" align="right"><input type="submit" value="{$REL_LANG->_('Save')}"/></td></tr>
+</table>
+</form>
