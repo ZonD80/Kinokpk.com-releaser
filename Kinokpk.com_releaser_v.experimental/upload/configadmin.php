b34b572aa917f850b80bb9ab52d6807a6bcd8133
ZonD80 <zond80@gmail.com>
2011-08-25 16:20:15 +0400

    XBT configuration GUI added

--- Kinokpk.com_releaser_v.experimental/upload/configadmin.php
+++ Kinokpk.com_releaser_v.experimental/upload/configadmin.php
@@ -22,6 +22,11 @@ if (!isset($_GET['action'])){
 	$tree = make_tree();
 	$REL_TPL->assign('pron_selector',gen_select_area('pron_cats', $tree,$REL_CONFIG['pron_cats'],false,true));
 	$REL_TPL->assign('REL_CACHEDRIVER',REL_CACHEDRIVER);
+	$xbt = $REL_DB->query_return("SELECT * FROM xbt_config");
+	foreach ($xbt as $xbtconfrow) {
+		$xbtconf[$xbtconfrow['name']] = $xbtconfrow['value'];
+	}
+	$REL_TPL->assign('xbt',$xbtconf);
 	$REL_TPL->output();
 	$REL_TPL->end_frame();
 	$REL_TPL->stdfoot();
@@ -33,7 +38,7 @@ elseif ($_GET['action'] == 'save'){
 'yourcopy','pm_max','default_language','static_language','siteonline','cache_template','cache_template_time',
 'avatar_max_width','avatar_max_height','default_theme','use_dc','deny_signup','allow_invite_signup',
 'use_ttl','use_email_act','use_captcha','use_blocks','use_gzip','use_ipbans',
-'as_timeout','as_check_messages','debug_mode','debug_language','debug_template','announce_packed','pron_cats','register_timezone','site_timezone','low_comment_hide','sign_length','default_notifs','default_emailnotifs','use_kinopoisk_trailers');
+'as_timeout','as_check_messages','debug_mode','debug_language','debug_template','pron_cats','register_timezone','site_timezone','low_comment_hide','sign_length','default_notifs','default_emailnotifs','use_kinopoisk_trailers','use_xbt');
 	$captcha_param = array('re_publickey','re_privatekey');
 
 	$updateset = array();
@@ -48,6 +53,12 @@ elseif ($_GET['action'] == 'save'){
 			if (!$_POST[$param] || !isset($_POST[$param])) stderr($REL_LANG->say_by_key('error'),"Приватный или публичный ключи капчи не определены");
 			$updateset[] = "UPDATE cache_stats SET cache_value=".sqlesc($_POST[$param])." WHERE cache_name='$param'";
 		}
+	}
+	
+	$xbt = (array)$_POST['xbt'];
+	$xbt = array_map('strval',$xbt);
+	foreach ($xbt AS $key=>$value) {
+		$updateset[] = "UPDATE xbt_config SET value=".sqlesc($value)." WHERE name=".sqlesc($key);
 	}
 
 	foreach ($updateset as $query) sql_query($query);
