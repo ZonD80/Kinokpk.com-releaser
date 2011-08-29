ba4b42f245100f32c1e621d84febe68f01b7283b
ZonD80 <zond80@gmail.com>
2011-08-29 16:55:37 +0400

    Templatesadmin translated

--- Kinokpk.com_releaser_v.experimental/upload/templatesadmin.php
+++ Kinokpk.com_releaser_v.experimental/upload/templatesadmin.php
@@ -17,13 +17,13 @@ get_privilege('edit_site_templates');
 httpauth();
 
 if (!isset($_GET['action'])) {
-	$REL_TPL->stdhead("Админка шкурок");
-	$REL_TPL->begin_frame("Администрация шкурок");
-	$res = sql_query("SELECT * FROM stylesheets ORDER BY id DESC");
-	print('<div align="center"><a href="'.$REL_SEO->make_link('templatesadmin','action','add').'">Добавить новый шаблон в базу</a></div>');
-	print('<table width="100%" border="1"><tr><td class="colhead">ID</td><td class="colhead">URI</td><td class="colhead">Название</td><td class="colhead">Редактировать</td></tr>');
+	$REL_TPL->stdhead($REL_LANG->_("Skins administration"));
+	$REL_TPL->begin_frame($REL_LANG->_("Skins administration"));
+	$res = $REL_DB->query("SELECT * FROM stylesheets ORDER BY id DESC");
+	print('<div align="center"><a href="'.$REL_SEO->make_link('templatesadmin','action','add').'">'.$REL_LANG->_('Register new template').'</a></div>');
+	print('<table width="100%" border="1"><tr><td class="colhead">ID</td><td class="colhead">URI</td><td class="colhead">'.$REL_LANG->_('Name').'</td><td class="colhead">'.$REL_LANG->_('Edit').'</td></tr>');
 	while ($row = mysql_fetch_array($res)){
-		print("<tr><td>{$row['id']}</td><td>{$row['uri']}</td><td>{$row['name']}</td><td><a href=\"".$REL_SEO->make_link('templatesadmin','action','edit','id',$row['id'])."\">Ред.</a> / <a onClick=\"return confirm('Вы уверены?')\" href=\"".$REL_SEO->make_link('templatesadmin','action','delete','id',$row['id'])."\">Уд.</a></td></tr>");
+		print("<tr><td>{$row['id']}</td><td>{$row['uri']}</td><td>{$row['name']}</td><td><a href=\"".$REL_SEO->make_link('templatesadmin','action','edit','id',$row['id'])."\">{$REL_LANG->_('Edit')}</a> / <a onClick=\"return confirm('{$REL_LANG->_('Are you sure?')}');\" href=\"".$REL_SEO->make_link('templatesadmin','action','delete','id',$row['id'])."\">{$REL_LANG->_('Delete')}</a></td></tr>");
 	}
 	print("</table>");
 
@@ -32,57 +32,57 @@ if (!isset($_GET['action'])) {
 }
 
 elseif ($_GET['action'] == 'add') {
-	$REL_TPL->stdhead("Админка шкурок: добавление новой шкурки");
-	$REL_TPL->begin_frame("Новая шкурка");
+	$REL_TPL->stdhead($REL_LANG->_('Registering new template'));
+	$REL_TPL->begin_frame($REL_LANG->_('Registering new template'));
 	print('<table width="400px"><form action="'.$REL_SEO->make_link('templatesadmin','action','saveadd').'" method="POST">
-    <tr><td>URI</td><td><input type="text" size="20" name="uri"></td></tr>
-    <tr><td>Название</td><td><input type="text" size="20" name="name"></td></tr><tr><td><input type="submit" value="Добавить"></td></tr></form></table>');
+    <tr><td>URI<br/>*'.$REL_LANG->_('Upload your template to themes/%URI% folder and specify URI here. Case sensitive').'</td><td><input type="text" size="20" name="uri"></td></tr>
+    <tr><td>'.$REL_LANG->_('Name').'</td><td><input type="text" size="20" name="name"></td></tr><tr><td><input type="submit" value="'.$REL_LANG->_('Add').'"></td></tr></form></table>');
 	$REL_TPL->end_frame();
 	$REL_TPL->stdfoot();
 }
 
 elseif ($_GET['action'] == 'saveadd') {
-	if (empty($_POST['name']) || empty($_POST['uri'])) stderr($REL_LANG->say_by_key('error'),"Вы не ввели URI либо Название шкурки");
+	if (empty($_POST['name']) || empty($_POST['uri'])) $REL_TPL->stderr($REL_LANG->say_by_key('error'),$REL_LANG->_('Missing form data'));
 
-	sql_query("INSERT INTO stylesheets (uri,name) VALUES (".sqlesc(htmlspecialchars((string)$_POST['uri'])).",".sqlesc(htmlspecialchars((string)$_POST['name'])).")");
+	$REL_DB->query("INSERT INTO stylesheets (uri,name) VALUES (".$REL_DB->sqlesc(htmlspecialchars((string)$_POST['uri'])).",".$REL_DB->sqlesc(htmlspecialchars((string)$_POST['name'])).")");
 	safe_redirect($REL_SEO->make_link('templatesadmin'));
 }
 
 elseif ($_GET['action'] == 'delete') {
-	if (!is_valid_id($_GET['id'])) stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
+	if (!is_valid_id($_GET['id'])) $REL_TPL->stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
 
-	sql_query("DELETE FROM stylesheets WHERE id={$_GET['id']} LIMIT 1");
+	$REL_DB->query("DELETE FROM stylesheets WHERE id={$_GET['id']} LIMIT 1");
 	safe_redirect($REL_SEO->make_link('templatesadmin'));
 }
 
 elseif ($_GET['action'] == 'edit') {
-	if (!is_valid_id($_GET['id'])) stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
+	if (!is_valid_id($_GET['id'])) $REL_TPL->stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
 	$id=(int)$_GET['id'];
 
-	$res = sql_query("SELECT * FROM stylesheets WHERE id=$id");
+	$res = $REL_DB->query("SELECT * FROM stylesheets WHERE id=$id");
 	$row = mysql_fetch_array($res);
-	if (!$row) stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
+	if (!$row) $REL_TPL->stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
 
-	$REL_TPL->stdhead("Админка шкурок: редактирование шкурки");
-	$REL_TPL->begin_frame("Редактор шкурки");
+	$REL_TPL->stdhead($REL_LANG->_('Editing a template'));
+	$REL_TPL->begin_frame($REL_LANG->_('Editing a template'));
 	print('<table width="400px"><form action="'.$REL_SEO->make_link('templatesadmin','action','saveedit','id',$id).'" method="POST">
-    <tr><td>URI</td><td><input type="text" size="20" name="uri" value="'.$row['uri'].'"></td></tr>
-    <tr><td>Название</td><td><input type="text" size="20" name="name" value="'.$row['name'].'"></td></tr><tr><td><input type="submit" value="Отредактировать"></td></tr></form></table>');
+    <tr><td>URI<br/>*'.$REL_LANG->_('Upload your template to themes/%URI% folder and specify URI here. Case sensitive').'</td><td><input type="text" size="20" name="uri" value="'.$row['uri'].'"></td></tr>
+    <tr><td>'.$REL_LANG->_('Name').'</td><td><input type="text" size="20" name="name" value="'.$row['name'].'"></td></tr><tr><td><input type="submit" value="'.$REL_LANG->_('Save changes').'"></td></tr></form></table>');
 	$REL_TPL->end_frame();
 	$REL_TPL->stdfoot();
 
 }
 
 elseif ($_GET['action'] == 'saveedit') {
-	if (!is_valid_id($_GET['id'])) stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
+	if (!is_valid_id($_GET['id'])) $REL_TPL->stderr($REL_LANG->say_by_key('error'),$REL_LANG->say_by_key('invalid_id'));
 	$id=(int)$_GET['id'];
 
-	if (empty($_POST['name']) || empty($_POST['uri'])) stderr($REL_LANG->say_by_key('error'),"Вы не ввели URI либо Название шкурки");
+	if (empty($_POST['name']) || empty($_POST['uri'])) $REL_TPL->stderr($REL_LANG->say_by_key('error'),$REL_LANG->_('Missing form data'));
 
-	sql_query("UPDATE stylesheets SET uri=".sqlesc(htmlspecialchars((string)$_POST['uri'])).", name=".sqlesc(htmlspecialchars((string)$_POST['name']))." WHERE id=$id");
+	$REL_DB->query("UPDATE stylesheets SET uri=".$REL_DB->sqlesc(htmlspecialchars((string)$_POST['uri'])).", name=".$REL_DB->sqlesc(htmlspecialchars((string)$_POST['name']))." WHERE id=$id");
 	safe_redirect($REL_SEO->make_link('templatesadmin'));
 
 }
-else stderr($REL_LANG->say_by_key('error'),"Unknown action");
+else $REL_TPL->stderr($REL_LANG->say_by_key('error'),$REL_LANG->_("Unknown action"));
 
 ?>
