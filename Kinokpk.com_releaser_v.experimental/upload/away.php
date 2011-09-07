a3540985e0b1ee70edaf56a74cc1f0ecf435c21a
ZonD80 <zond80@gmail.com>
2011-09-07 17:57:27 +0400

    away.php translated

--- Kinokpk.com_releaser_v.experimental/upload/away.php
+++ Kinokpk.com_releaser_v.experimental/upload/away.php
@@ -18,17 +18,13 @@ print('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://ww
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="Description" content="'.$REL_CONFIG['description'].'" />
 <meta name="Keywords" content="'.$REL_CONFIG['keywords'].'" />
-<title>'.$REL_CONFIG['sitename'].' - Переход по внешней ссылке</title>
+<title>'.$REL_CONFIG['sitename'].' - '.$REL_LANG->_('Going to external link').'</title>
 </head>
 <body  style="padding:20px 180px; font-size:12px; font-family:Tahoma; line-height:200%">
-<h2>'.$REL_CONFIG['sitename'].' - Переход по внешней ссылке</h2>
-
-Вы покидаете сайт '.$REL_CONFIG['sitename'].' по внешней ссылке <b>'.urldecode($url).'</b>, предоставленной одним из участников. <br/>
-<a href="'.$REL_SEO->make_link('staff').'">Администрация</a> '.$REL_CONFIG['sitename'].' не несет ответственности за содержимое сайта <b>'.urldecode($url).'</b> и настоятельно рекомендует <b>не указывать</b> никаких своих данных, имеющих отношение к '.$REL_CONFIG['sitename'].' (особенно <b>e-mail</b>, <b>пароль</b> и <b>cookies</b>), на сторонних сайтах. <br/><br/>
-Кроме того, сайт <b>'.urldecode($url).'</b> может содержать вирусы, трояны и другие вредоносные программы, опасные для Вашего компьютера. <br/>
-Если у Вас нет серьезных оснований доверять этому сайту, лучше всего на него не переходить, даже если Вы якобы получили эту ссылку от одного из Ваших <a href="'.$REL_SEO->make_link('friends').'">друзей</a>. <br/><br/>
-Если Вы еще не передумали, нажмите на <a href="'.$url.'" id="to_go">'.urldecode($url).'</a>. <br/>
-Если Вы не хотите рисковать безопасностью Вашего аккаунта и компьютера, нажмите <a href="javascript:history.go(-1);">отмена</a>.
+<h2>'.$REL_CONFIG['sitename'].' - '.$REL_LANG->_('Going to external link').'</h2>');
+$REL_TPL->assign('url',$url);
+$REL_TPL->output('index_'.getlang());
+print ('
 </body>
 </html>
 ');
